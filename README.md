07d58b59c8e44d3f78d883185c0a59af29dadad6
7385810 <mandemons1@hotmail.com>
2021-03-15 21:46:54 +0100

    feat: laravel api sample

---
+++ README.md
@@ -0,0 +1,2 @@
+# laravel-docker-sample-api
+A simple project to create a laravel api with docker container
---
+++ src/app/Http/Resources/Topicality.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Topicality extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => 'Titre de mon actualité : ' . $this->title,
+            'content' => substr($this->content, 0, 20) . '...'
+        ];
+    }
+}
